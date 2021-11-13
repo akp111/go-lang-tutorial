b891647c3e59e92f78544fa50fbcd4900417c0b5
akp111 <pradhanashish95@gmail.com>
2021-11-13 12:38:42 +0530

    go tutorial

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+// Declare a main package (a package is a way to group functions, and it's made up of all the files in the same directory)
+package main
+
+// Import the popular fmt package, which contains functions for formatting text, including printing to the console.
+import "fmt"
+// For using external packages
+import "rsc.io/quote"
+//Implement a main function to print a message to the console. A main function executes by default when you run the main package.
+func main() {
+    fmt.Println(quote.Go())
+}
